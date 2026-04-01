1afb69aa71bb9daf7d840e83412358edf047a602
matiashgarcia <matiashgarcia@uade.edu.ar>
2026-04-01 02:25:32 -0300

    Added ejercicios folder to practice exercises

--- test/Test.java
+++ test/Test.java
@@ -1,5 +1,6 @@
 package test;
 
+import ejercicios.PasarColaACola;
 import especificaciones.ColaTDA;
 import especificaciones.SemaforoTDA;
 import implEstaticas.Cola;
@@ -8,11 +9,17 @@ import implEstaticas.Semaforo;
 public class Test {
     public static void main(String[] args) {
         ColaTDA cola = new Cola();
+        ColaTDA cola2 = new Cola();
         cola.inicializarCola();
+        cola2.inicializarCola();
         cola.acolar(5);
+        cola.acolar(3);
+        PasarColaACola.PasarColaACola(cola, cola2);
         System.out.println(cola.primero());
         cola.desacolar();
         System.out.println(cola.primero());
-
+        System.out.println(cola2.primero());
+        cola2.desacolar();
+        System.out.println(cola2.primero());
     }
 }
