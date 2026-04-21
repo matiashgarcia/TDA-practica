9f9cbdde49b14e33263d4a66ef861b6862d94e41
matiashgarcia <matiashgarcia@uade.edu.ar>
2026-04-21 20:06:59 -0300

    Finished diccionarioMultipleTDA

--- tests/DiccionarioMultipleTest.java
+++ tests/DiccionarioMultipleTest.java
@@ -2,16 +2,30 @@ package tests;
 
 import ejercicios.DiccionarioMultiplePractica;
 import especificaciones.DiccionarioMultipleTDA;
+import especificaciones.DiccionarioSimpleTDA;
 import implEstaticas.DiccionarioMultiple;
+import implEstaticas.DiccionarioSimple;
 
 public class DiccionarioMultipleTest {
 
     public static void main(String[] args) {
-        DiccionarioMultipleTDA d = new DiccionarioMultiple();
+/*         DiccionarioMultipleTDA d = new DiccionarioMultiple();
         d.inicializarDiccionario();
         d.agregar(2, 5);
         d.agregar(91, 6);
+        d.agregar(91, 12);
+        d.agregar(91, 82);
         d.agregar(8, 53);
-        DiccionarioMultiplePractica.imprimirDiccionario(d);
+        d.agregar(8, 20);
+        DiccionarioMultiplePractica.imprimirDiccionario(d); */
+
+        DiccionarioSimpleTDA dicSimple = new DiccionarioSimple();
+        dicSimple.inicializarDiccionario();
+        dicSimple.agregar(2, 5);
+        dicSimple.agregar(94, 41);
+        dicSimple.agregar(62, 4);
+        dicSimple.agregar(1, 5);
+        DiccionarioMultipleTDA d2 = DiccionarioMultiplePractica.pasarDicSimpleADicMult(dicSimple);
+        DiccionarioMultiplePractica.imprimirDiccionario(d2);
     }
 }
